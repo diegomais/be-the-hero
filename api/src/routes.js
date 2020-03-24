08d84b3a1dade33c305304b30cd8b721940f84a0
Diego Mais <diegomais.dev@gmail.com>
2020-03-24 16:42:02 -0300

    [feat] Create NGO User should create an NGO profile to sign up to app
    Changes to be committed:
    	modified:   src/routes.js
    	modified:   src/database/db.sqlite
    	src/database/connection.js

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => res.send('Be the hero!'));
+routes.post('/ngos', async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body;
+
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ngos').insert({ id, name, email, whatsapp, city, uf });
+
+  return res.json({ id });
+});
 
 module.exports = routes;
