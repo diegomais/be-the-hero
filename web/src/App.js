3fb7c4f848668d9dbd413c7d18c2fdc36d1fad6a
Diego Mais <diegomais.dev@gmail.com>
2020-03-25 13:35:15 -0300

    [feat] Add routing User should access different routes in app
    Changes to be committed:
    	modified:   package.json
    	modified:   src/App.js
    	new file:   src/routes.js
    	modified:   yarn.lock

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Logon from './pages/Logon';
+
+import Routes from './routes';
 
 import './global.css';
 
 function App() {
-  return <Logon />;
+  return <Routes />;
 }
 
 export default App;
