bdaed45bfa14185913360cb3d1094fc535c5666a
Diego Mais <diegomais.dev@gmail.com>
2020-03-25 10:40:44 -0300

    [style] Add ESLint, Prettier & EditorConfig
    1 - generate .editorconfig file
    2 - add eslint as development dependency
    3 - eslint --init to set up airbnb style guide
    4 - delete package-lock.json
    5 - yarn install to update dependencies
    6 - create .prettierrc
    7 - add prettier eslint-config-prettier eslint-plugin-prettier eslint-plugin-react-hooks babel-eslint as development dependencies
    8 - set up .eslintrc.js to prettier eslint-config-prettier eslint-plugin-prettier eslint-plugin-react-hooks babel-eslint
    
    Changes to be committed:
    	new file:   .editorconfig
    	new file:   .eslintrc.js
    	new file:   .prettierrc
    	modified:   package.json
    	modified:   src/App.js
    	modified:   yarn.lock

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React from 'react';
+import Header from './Header';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return <Header />;
 }
 
 export default App;
