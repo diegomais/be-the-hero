09879b68910c47e8978a9dbd40cf5b0112d0a1a9
Diego Mais <diegomais.dev@gmail.com>
2020-03-25 18:17:32 -0300

    [feat](web) Add requests to api on sign up page
    Changes to be committed:
    	modified:   package.json
    	modified:   src/pages/Register/index.js
    	new file:   src/services/api.js
    	modified:   yarn.lock

--- web/src/pages/Register/index.js
+++ web/src/pages/Register/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 
+import api from '../../services/api';
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
 
 export default function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [city, setCity] = useState('');
+  const [uf, setUf] = useState('');
+
+  const history = useHistory();
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    const data = { name, email, whatsapp, city, uf };
+
+    try {
+      const response = await api.post('ngos', data);
+
+      alert(
+        `Your access ID: ${response.data.id}. Write it down because it is necessary to access the platform.`
+      );
+
+      history.push('/');
+    } catch (error) {
+      alert('There was a problem. Please try again later.');
+    }
+  }
+
   return (
     <div className="register-container">
       <div className="content">
@@ -25,13 +52,35 @@ export default function Register() {
           </Link>
         </section>
 
-        <form>
-          <input placeholder="Name of NGO" />
-          <input type="email" placeholder="Email" />
-          <input placeholder="WhatsApp" />
+        <form onSubmit={handleRegister}>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Name of NGO"
+          />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            type="email"
+          />
+          <input
+            value={whatsapp}
+            onChange={(e) => setWhatsapp(e.target.value)}
+            placeholder="WhatsApp"
+          />
           <div className="input-group">
-            <input placeholder="City" />
-            <input placeholder="UF" style={{ width: 80 }} />
+            <input
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              placeholder="City"
+            />
+            <input
+              value={uf}
+              onChange={(e) => setUf(e.target.value)}
+              placeholder="UF"
+              style={{ width: 80 }}
+            />
           </div>
           <button className="button" type="submit">
             Sign Up
