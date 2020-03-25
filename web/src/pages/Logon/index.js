8c78150ffd4db5c51a61fcf5aa522a7a0f9703ca
Diego Mais <diegomais.dev@gmail.com>
2020-03-25 18:39:31 -0300

    [feat](web) Add requests to api on logon component
    Changes to be committed:
    	modified:   src/pages/Logon/index.js

--- web/src/pages/Logon/index.js
+++ web/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
@@ -8,15 +10,37 @@ import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
+  const [id, setId] = useState('');
+
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    try {
+      const response = await api.post('sessions', { id });
+
+      localStorage.setItem('be-the-hero-id', id);
+      localStorage.setItem('be-the-hero-name', response.data.name);
+
+      history.push('/profile');
+    } catch (error) {
+      alert('There was an error. Please try again later.');
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Sign In</h1>
 
-          <input placeholder="Your ID" />
+          <input
+            value={id}
+            onChange={e => setId(e.target.value)}
+            placeholder="Your ID"
+          />
           <button className="button" type="submit">
             Sign In
           </button>
