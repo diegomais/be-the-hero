d06a93cda26e134ab28b1aa75288c50d5d2346ed
Diego Mais <diegomais.dev@gmail.com>
2020-03-26 18:46:39 -0300

    [feat](mobile) Add linting tools and routing
    Changes to be committed:
    	new file:   .editorconfig
    	new file:   .eslintrc.js
    	new file:   .prettierrc
    	modified:   App.js
    	modified:   package.json
    	new file:   src/pages/Details/index.js
    	new file:   src/pages/Details/styles.js
    	new file:   src/pages/Incidents/index.js
    	new file:   src/pages/Incidents/styles.js
    	new file:   src/routes.js
    	modified:   yarn.lock

---
+++ mobile/src/pages/Details/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Details() {
+  return (
+    <View>
+      <Text>Details</Text>
+    </View>
+  );
+}
---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Incidents() {
+  return (
+    <View>
+      <Text>Incidents</Text>
+    </View>
+  );
+}
