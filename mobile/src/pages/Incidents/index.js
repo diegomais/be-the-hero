8e7d72f5d8dfb05149c29541408fffaac37256c2
Diego Mais <diegomais.dev@gmail.com>
2020-03-26 21:51:29 -0300

    [feat](mobile) Add requests to api from incidents User should see incidents inserted in database
    Changes to be committed:
    	modified:   App.js
    	modified:   package.json
    	modified:   src/pages/Incidents/index.js
    	new file:   src/services/api.js
    	modified:   yarn.lock

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 
 import styles from './styles';
-
+import api from '../../services/api';
 import logoImg from '../../assets/logo.png';
 
 export default function Incidents() {
+  const [incidents, setIncidents] = useState([]);
+  const [incidentsTotal, setIncidentsTotal] = useState(0);
+
   const navigation = useNavigation();
 
-  function navigateToDetail() {
-    navigation.navigate('Details');
+  async function loadIncidents() {
+    const response = await api.get('incidents');
+
+    setIncidents(response.data);
+    setIncidentsTotal(response.headers['x-total-count']);
+  }
+
+  useEffect(() => {
+    loadIncidents();
+  }, []);
+
+  function navigateToDetail(incident) {
+    navigation.navigate('Details', { incident });
   }
 
   return (
@@ -19,7 +33,8 @@ export default function Incidents() {
       <View style={styles.header}>
         <Image source={logoImg} />
         <Text style={styles.headerText}>
-          Total of <Text style={styles.headerTextBold}>0 incidents</Text>
+          Total of{' '}
+          <Text style={styles.headerTextBold}>{incidentsTotal} incidents</Text>
         </Text>
       </View>
 
@@ -30,21 +45,26 @@ export default function Incidents() {
 
       <FlatList
         style={styles.incidentList}
-        data={[1, 2, 3]}
-        keyExtractor={(incident) => String(incident)}
+        data={incidents}
+        keyExtractor={(incident) => String(incident.id)}
         showsVerticalScrollIndicator={false}
-        renderItem={() => (
+        renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>NGO:</Text>
-            <Text style={styles.incidentValue}>AJAPRA</Text>
+            <Text style={styles.incidentValue}>{incident.name}</Text>
             <Text style={styles.incidentProperty}>INCIDENT:</Text>
-            <Text style={styles.incidentValue}>Cachorro atropelado</Text>
+            <Text style={styles.incidentValue}>{incident.title}</Text>
             <Text style={styles.incidentProperty}>VALUE:</Text>
-            <Text style={styles.incidentValue}>R$ 120,00</Text>
+            <Text style={styles.incidentValue}>
+              {Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              }).format(incident.value)}
+            </Text>
             <TouchableOpacity
               style={styles.detailsButton}
               onPress={() => {
-                navigateToDetail();
+                navigateToDetail(incident);
               }}
             >
               <Text style={styles.detailsButtonText}>Further details</Text>
