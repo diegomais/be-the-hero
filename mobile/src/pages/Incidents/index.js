ce82ff565e2748eece2510eb33f3f46e065de402
Diego Mais <diegomais.dev@gmail.com>
2020-03-26 22:00:36 -0300

    [feat](mobile) Add pagination on Incidents list
    Changes to be committed:
    	modified:   src/pages/Incidents/index.js

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -10,14 +10,28 @@ import logoImg from '../../assets/logo.png';
 export default function Incidents() {
   const [incidents, setIncidents] = useState([]);
   const [incidentsTotal, setIncidentsTotal] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   const navigation = useNavigation();
 
   async function loadIncidents() {
-    const response = await api.get('incidents');
+    if (loading) {
+      return;
+    }
 
-    setIncidents(response.data);
+    if (incidentsTotal > 0 && incidentsTotal === incidents.length) {
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.get('incidents', { params: { page } });
+
+    setIncidents([...incidents, ...response.data]);
     setIncidentsTotal(response.headers['x-total-count']);
+    setPage(page + 1);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -48,6 +62,8 @@ export default function Incidents() {
         data={incidents}
         keyExtractor={(incident) => String(incident.id)}
         showsVerticalScrollIndicator={false}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.2}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>NGO:</Text>
