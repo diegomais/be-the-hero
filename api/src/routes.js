0e759dec170d2aef9499055b903af17dda8c3e91
Diego Mais <diegomais.dev@gmail.com>
2020-03-27 11:41:22 -0300

    [WIP](api) Add validation on some routes Prevent bad requests to api
    Changes to be committed:
    	modified:   package.json
    	modified:   src/index.js
    	modified:   src/routes.js
    	modified:   yarn.lock

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const NgoController = require('./controllers/NgoController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -9,12 +10,48 @@ const routes = express.Router();
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ngos', NgoController.index);
-routes.post('/ngos', NgoController.create);
+routes.post(
+  '/ngos',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required().min(10).max(11),
+      city: Joi.string().required(),
+      uf: Joi.string().required().length(2),
+    }),
+  }),
+  NgoController.create
+);
 
-routes.get('/profile', ProfileController.index);
+routes.get(
+  '/profile',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  ProfileController.index
+);
 
-routes.get('/incidents', IncidentController.index);
+routes.get(
+  '/incidents',
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+    }),
+  }),
+  IncidentController.index
+);
 routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.destroy);
+routes.delete(
+  '/incidents/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+  }),
+  IncidentController.destroy
+);
 
 module.exports = routes;
