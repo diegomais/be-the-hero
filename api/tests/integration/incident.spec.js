67fb196213cc4676fc732ac1b061b059e5a65c3e
Diego Mais <diegomais.dev@gmail.com>
2020-03-29 21:32:27 -0300

    [test](api) Add tests to ngos and incidents routes - User should be able to create a new NGO - User should be able to list registered NGOs - User should be able to create a new incident - User should be able to list incidents - API should return an error if deleting an nonexistent incident - Another NGO should not be able to delete an incident - API should return status 200 after deleting an incident
    Changes to be committed:
    	new file:   tests/integration/incident.spec.js
    	modified:   tests/integration/ngo.spec.js

---
+++ api/tests/integration/incident.spec.js
@@ -0,0 +1,73 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+const generateUniqueId = require('../../src/utils/generateUniqueId');
+
+const ngo1_data = {
+  name: 'Abrigo de Animais Au Family',
+  email: 'contato.aufamilyabrigo@gmail.com',
+  whatsapp: '91985255716',
+  city: 'Belém',
+  uf: 'PA',
+};
+
+const incident1_data = {
+  title: 'Novo Abrigo Au Family',
+  description:
+    'Somos um abrigo de animais de Belém do Pará que cuida de quase 700 animais vitimas de maus tratos. Hoje estamos vivendo o nosso maior desafio, que é construir um novo abrigo para todos os nossos animais em apenas 120 dias.',
+  value: 200,
+};
+let responseCreateNgo = {};
+let responseCreateIncident = {};
+
+describe('Incident', () => {
+  beforeAll(async () => {
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.migrate.rollback();
+    await connection.destroy();
+  });
+
+  it('should be able to create a new incident', async () => {
+    responseCreateNgo = await request(app).post('/ngos').send(ngo1_data);
+    responseCreateIncident = await request(app)
+      .post('/incidents')
+      .send(incident1_data)
+      .set('Authorization', String(responseCreateNgo.body.id));
+
+    expect(responseCreateIncident.body).toHaveProperty('id');
+  });
+
+  it('should be able to list incidents', async () => {
+    const responseListIncident = await request(app).get('/incidents');
+
+    expect(responseListIncident.body).toMatchObject([incident1_data]);
+  });
+
+  it('should return an error if deleting an nonexistent incident', async () => {
+    const response = await request(app)
+      .delete(`/incidents/0`)
+      .set('Authorization', responseCreateNgo.body.id);
+
+    expect(response.status).toBe(404);
+  });
+
+  it('another NGO should not be able to delete an incident', async () => {
+    const response = await request(app)
+      .delete(`/incidents/${responseCreateIncident.body.id}`)
+      .set('Authorization', generateUniqueId());
+
+    expect(response.status).toBe(401);
+  });
+
+  it('should return status 200 after deleting an incident', async () => {
+    const response = await request(app)
+      .delete(`/incidents/${responseCreateIncident.body.id}`)
+      .set('Authorization', responseCreateNgo.body.id);
+
+    expect(response.status).toBe(204);
+  });
+});
--- api/tests/integration/ngo.spec.js
+++ api/tests/integration/ngo.spec.js
@@ -2,8 +2,30 @@ const request = require('supertest');
 const app = require('../../src/app');
 const connection = require('../../src/database/connection');
 
+const ngo1_data = {
+  name: 'AJAPRA - Associação Jaraguaense Protetora dos Animais',
+  email: 'ajapra@ajapra.org.br',
+  whatsapp: '47916420136',
+  city: 'Jaraguá do Sul',
+  uf: 'SC',
+};
+const ngo2_data = {
+  name: 'Anjos de Patas - Proteção Animal',
+  email: 'orandilia.faria@hotmail.com',
+  whatsapp: '47912345678',
+  city: 'Porto Belo',
+  uf: 'SC',
+};
+const ngo3_data = {
+  name: 'Associação Viva Bicho de Proteção aos Animais',
+  email: 'vivabicho@vivabicho.org',
+  whatsapp: '4732631020',
+  city: 'Balneário Camboriú',
+  uf: 'SC',
+};
+
 describe('NGO', () => {
-  beforeEach(async () => {
+  beforeAll(async () => {
     await connection.migrate.rollback();
     await connection.migrate.latest();
   });
@@ -14,15 +36,20 @@ describe('NGO', () => {
   });
 
   it('should be able to create a new NGO', async () => {
-    const response = await request(app).post('/ngos').send({
-      name: 'AJAPRA',
-      email: 'contato@ajapra.com.br',
-      whatsapp: '47912345678',
-      city: 'Jaraguá do Sul',
-      uf: 'SC',
-    });
+    const response = await request(app).post('/ngos').send(ngo1_data);
 
     expect(response.body).toHaveProperty('id');
     expect(response.body.id).toHaveLength(8);
+  });
+
+  it('should be able to list registered NGOs', async () => {
+    await request(app).post('/ngos').send(ngo1_data);
+    await request(app).post('/ngos').send(ngo2_data);
+    await request(app).post('/ngos').send(ngo3_data);
+    const response = await request(app).get('/ngos');
+
+    expect(response.body).toEqual(
+      expect.arrayContaining([ngo1_data, ngo2_data, ngo3_data])
+    );
   });
 });
