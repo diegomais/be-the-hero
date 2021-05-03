dd163f70b287040d9ea63958b3d24bb8ae3700a2
Diego Mais <diegomais.dev@gmail.com>
2021-05-02 23:35:07 -0300

    🏷️ (frontend): fix type warnings

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,7 +1,6 @@
+import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />
 }
-
-export default App
